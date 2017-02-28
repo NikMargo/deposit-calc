1e0a1014410aedbcddfa451d220d9de5fc0551e0
NikMargo <Ritosik25@yandex.ru>
2017-02-28 17:55:05 +0700

    created function rascheta

--- 2lab.cpp
+++ 2lab.cpp
@@ -10,4 +10,23 @@ int vvod(int* sr, double* sv)
     cout<<"Summa vklada:";
     cin>>*sv;
 }
+int raschet(int* sr, double* sv)
+{	
+    if (*sr>0 && *sr<=30){
+ 	    *sv*=0.9;
+    }
+    
+    if (*sr>=31 && *sr<=120){
+ 	    *sv*=1.02;
+    }
+    
+    if (*sr>=121 && *sr<=240){
+ 	    *sv*=1.06;
+    }
+    
+    if (*sr>=241 && *sr<=365){
+	    *sv*=1.12;
+	}
+ 	   
+}
 
