9374c98fbaebbcbd98152cf40d713aca389524b7
NikMargo <Ritosik25@yandex.ru>
2017-02-28 17:47:14 +0700

    create function vvod

---
+++ 2lab.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int vvod(int* sr, double* sv)
+{
+    cout<<"Srok vklada:";
+    cin>>*sr;
+    cout<<"Summa vklada:";
+    cin>>*sv;
+}
+
